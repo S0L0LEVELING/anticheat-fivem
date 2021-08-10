4c7e350e441be28b1d62120307d639ed649f0a3b
AYROX <76447939+FISHAYROX@users.noreply.github.com>
2021-08-11 01:26:38 +0200

    Upload

---
+++ AyroxShield/configs/config_sv.lua
@@ -0,0 +1,8 @@
+ConfigACS = {}
+
+ConfigACS.LogBanWebhook = "" -- WEBHOOK COMMUNAUTÉ
+ConfigACS.LogHRPWebhook = "" --WEBHOOK BANS / LOGS
+
+ConfigACS.ServerName = "NOM DU SERVEUR" -- LE NOM DU SERVEUR
+
+ConfigACS.Version = "v1.1.0" -- NE PAS TOUCHER
